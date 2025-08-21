829e57875a061372a0cb53a0cf3016ed81b38175
SooBiehn Hah <biehn.hah@gmail.com>
2025-08-21 11:34:16 +0900

    first commit

---
+++ src/main/java/com/biehnary/auction/live_auction/entity/Member.java
@@ -0,0 +1,21 @@
+package com.biehnary.auction.live_auction.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private LocalDateTime joinDate;
+    private Boolean isActive;
+
+}
