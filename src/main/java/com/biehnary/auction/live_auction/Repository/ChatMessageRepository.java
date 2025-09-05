1eb6298b9e10c22a2f2bee6525d967aacc173714
SooBiehn Hah <biehn.hah@gmail.com>
2025-09-05 14:11:47 +0900

    add ChatMessageRepository

---
+++ src/main/java/com/biehnary/auction/live_auction/Repository/ChatMessageRepository.java
@@ -0,0 +1,44 @@
+package com.biehnary.auction.live_auction.Repository;
+
+import com.biehnary.auction.live_auction.entity.ChatMessage;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ChatMessageRepository {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  public ChatMessage save(ChatMessage message) {
+    em.persist(message);
+    return message;
+  }
+
+  // == find tools ==
+  public List<ChatMessage> findRecentMessages(int limit) {
+    return em.createQuery("select c from ChatMessage c order by c.sendTime desc",
+            ChatMessage.class)
+        .setMaxResults(limit)
+        .getResultList();
+  }
+
+  public Long count() {
+    return em.createQuery("select count (c) from ChatMessage c", Long.class)
+        .getSingleResult();
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
